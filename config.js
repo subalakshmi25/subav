4a80c87ada1969e96f605d4edf1f988ff1ee0c60
subalakshmi25 <subalakshmivijayakumar69@gmail.com>
2020-03-20 18:22:35 +0530

    login files

---
+++ config.js
@@ -0,0 +1,17 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Subasundar@25',
+  database : 'test'   
+
+
+});    
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected");
+} else {
+    console.log("Error while connecting with database");
+}
+});
+module.exports = connection;
